5c9fa56d50f2224b391c5858e24c371bfc5321b2
philipp.minder <philipp.minder@supsign.ch>
2022-03-29 17:00:35 +0200

    wip

--- resources/views/livewire/select.blade.php
+++ resources/views/livewire/select.blade.php
@@ -1,11 +1,6 @@
 <div>
     <label class="select__label">{{$label}}</label>
     <select class="select__field">
-        @foreach($options as $option)
-            <option class="select__dropdown-option">
-                {{$option}}
-            </option>
 
-        @endforeach
     </select>
 </div>
