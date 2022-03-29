cec08dc7fc132e29525fded304c267c469b4dce7
Florian Ratz <florian.ratz@supsign.ch>
2022-03-29 11:41:35 +0200

    no message

---
+++ app/Http/Controllers/Admin/ConfigurationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\PostConfigurationRequest;
+
+class ConfigurationController extends Controller
+{
+    public function post(PostConfigurationRequest $request)
+    {
+
+    }
+
+    public function show()
+    {
+        return view('admin.config');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ConfigurationController;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome')->name('home');
+
+Route::get('admin/config', [ConfigurationController::class, 'show'])->name('admin.config.show');
 
 Route::get('test', [Controller::class, 'test'])->name('test');
