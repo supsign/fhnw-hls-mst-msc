74806a5ef60ad91f368397fdbdbee590d34e3c5b
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2022-03-30 11:37:12 +0200

    job done

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,19 +15,6 @@ require("laravel-mix-tailwind");
 mix.copyDirectory('resources/img/', 'public/img/');
 
 // // FontAwesome Main CSS + Webfonts / SVG
-mix.copyDirectory(
-    'node_modules/@fortawesome/fontawesome-pro/webfonts',
-    'public/vendors/fontawesome-pro/webfonts'
-);
-mix.copyDirectory(
-    'node_modules/@fortawesome/fontawesome-pro/svgs',
-    'public/vendors/fontawesome-pro/svgs'
-);
-
-mix.copy(
-    'node_modules/@fortawesome/fontawesome-pro/css/all.min.css',
-    'public/vendors/fontawesome-pro/css/all.min.css'
-);
 
 mix.js("resources/js/app.js", "public/js/app.js")
     .sass("resources/sass/app.scss", "public/css/app.css")
