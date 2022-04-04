fcf4be5ea880e52718eb6150d804f67464a73494
Florian Ratz <florian.ratz@supsign.ch>
2022-04-04 15:42:02 +0200

    rename #2

---
+++ app/Http/Livewire/ModuleSelectionForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ModuleSelectionForm extends Component
+{
+
+    public $semesters;
+    public $studyModes;
+    public $specializations;
+    public $semester;
+    public $specialization;
+
+    protected $listeners = ['changestart','changespecialization'];
+
+    public function changestart($selected) {
+        $this->semester = $selected;
+    }
+    public function changespecialization($selected) {
+        $this->specialization = $selected;
+    }
+
+
+    public function render()
+    {
+        return view('livewire.module-selection-form');
+    }
+}
