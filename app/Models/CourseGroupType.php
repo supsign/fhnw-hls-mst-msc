49d65a7423149505dbaf7bb3925ca9eb0e6d7e8f
Florian Ratz <florian.ratz@supsign.ch>
2022-04-04 14:27:03 +0200

    relations

--- app/Models/CourseGroupType.php
+++ app/Models/CourseGroupType.php
@@ -2,7 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class CourseGroupType extends BaseModel
 {
-
+    public function courseGroups(): HasMany
+    {
+        return $this->hasMany(CourseGroup::class);
+    }
 }
