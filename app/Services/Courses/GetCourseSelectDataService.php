82ee85c443cc5de75f80b407d40416eda7381271
Florian Ratz <florian.ratz@supsign.ch>
2022-04-13 14:13:31 +0200

    return empty array if no courseGroup

--- app/Services/Courses/GetCourseSelectDataService.php
+++ app/Services/Courses/GetCourseSelectDataService.php
@@ -14,32 +14,36 @@ class GetCourseSelectDataService
     protected ?Semester $semester;
     protected Specialization $specialization;
 
-    public function __invoke(CourseGroupType $courseGroupType, Specialization $specialization, Semester $semester = null)       //: array
+    public function __invoke(CourseGroupType $courseGroupType, Specialization $specialization, Semester $semester = null): array
     {
         $this->courseGroupType = $courseGroupType;
         $this->semester = $semester;
         $this->specialization = $specialization;
 
         if (!$this->semester) {
-            return $this->getCourseGroup()->toArray();
+            return $this->getCourseGroup()?->toArray() ?? [];
         }
 
-        return $this->getCourseGroupFilterBySemester();
+        return $this->getCourseGroupFilterBySemester()?->toArray() ?? [];
     }
 
-    protected function getCourseGroup(): CourseGroup
+    protected function getCourseGroup(): ?CourseGroup
     {
         return CourseGroupSpecialization::where('specialization_id', $this->specialization->id)
             ->join('course_groups', 'course_group_specialization.course_group_id', '=', 'course_groups.id')
             ->where('type', $this->courseGroupType->value)
             ->with(['courseGroup', 'courseGroup.courses', 'courseGroup.courses.semesters'])
             ->first()
-                ->courseGroup;
+                ?->courseGroup;
     }
 
-    protected function getCourseGroupFilterBySemester(): CourseGroup
+    protected function getCourseGroupFilterBySemester(): ?CourseGroup
     {
         $courseGroup = $this->getCourseGroup();
+
+        if (!$courseGroup) {
+            return null;
+        }
 
         $courseGroup->courses = $courseGroup->courses->filter(function ($course) {
             return $course->semesters->contains($this->semester);
