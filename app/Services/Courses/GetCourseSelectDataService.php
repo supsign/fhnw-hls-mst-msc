ad66b8922aa37970fd18f1aa3eb727d5c8ebcb60
Florian Ratz <florian.ratz@supsign.ch>
2022-04-19 10:39:39 +0200

    reintegrate semester filter

--- app/Services/Courses/GetCourseSelectDataService.php
+++ app/Services/Courses/GetCourseSelectDataService.php
@@ -16,32 +16,48 @@ class GetCourseSelectDataService
     protected ?Semester $semester;
     protected Specialization $specialization;
 
-    public function __invoke(CourseGroupType $courseGroupType, Specialization $specialization, Semester $semester = null, bool $invertSpecialization = false): array
-    {
+    public function __invoke(
+        CourseGroupType $courseGroupType, 
+        Specialization $specialization, 
+        Semester $semester = null, 
+        bool $invertSpecialization = false
+    ): array {
         $this->courseGroupType = $courseGroupType;
         $this->invertSpecialization = $invertSpecialization;
         $this->semester = $semester;
         $this->specialization = $specialization;
 
-        if (!$this->semester) {
-            if ($this->invertSpecialization) {
-                return $this->getCourses()->toArray();
-            }
-
-            return $this->getCourseGroup()?->toArray() ?? [];
+        if ($this->invertSpecialization) {
+            return $this->getCourses()->toArray() ?? [];
         }
 
-        return $this->getCourseGroupFilterBySemester()?->toArray() ?? [];
+        return $this->getCourseGroup()?->toArray() ?? [];
     }
 
     protected function getCourses(): Collection
     {
-        return $this->getCourseGroupSpecialization()->get()->pluck('courseGroup.courses')->flatten(1)->unique()->values();
+        $courses = $this->getCourseGroupSpecialization()->get()->pluck('courseGroup.courses')->flatten(1)->unique();
+
+        if ($this->semester) {
+            $courses = $courses->filter(function ($course) {
+                return $course->semesters->contains($this->semester);
+            });
+        }
+
+        return $courses->values();
     }
 
     protected function getCourseGroup(): ?CourseGroup
     {
-        return $this->getCourseGroupSpecialization()->first()?->courseGroup;   
+        $courseGroup = $this->getCourseGroupSpecialization()->first()?->courseGroup;
+
+        if ($this->semester && $courseGroup) {
+            $courseGroup->courses = $courseGroup->courses->filter(function ($course) {
+                return $course->semesters->contains($this->semester);
+            })->values();
+        }
+
+        return $courseGroup;
     }
 
     protected function getCourseGroupSpecialization()
@@ -56,20 +72,5 @@ class GetCourseSelectDataService
             })
             ->where('type', $this->courseGroupType->value)
             ->with(['courseGroup', 'courseGroup.courses', 'courseGroup.courses.semesters']);
-    }
-
-    protected function getCourseGroupFilterBySemester(): ?CourseGroup
-    {
-        $courseGroup = $this->getCourseGroup();
-
-        if (!$courseGroup) {
-            return null;
-        }
-
-        $courseGroup->courses = $courseGroup->courses->filter(function ($course) {
-            return $course->semesters->contains($this->semester);
-        })->values();
-
-        return $courseGroup;
     }
 }
