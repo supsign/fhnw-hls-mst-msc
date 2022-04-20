343ef33c1f8970b0a030c6885cdb0d6d1fe05840
Florian Ratz <florian.ratz@supsign.ch>
2022-04-20 11:13:10 +0200

    reset test controller

--- app/Http/Controllers/Admin/TestController.php
+++ app/Http/Controllers/Admin/TestController.php
@@ -2,42 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Enums\CourseGroupType;
 use App\Http\Controllers\Controller;
-use App\Models\CourseGroup;
-use App\Models\Specialization;
-use App\Services\Courses\GetCourseSelectDataService;
 
 class TestController extends Controller
 {
-    public function test(GetCourseSelectDataService $getCourseSelectDataService): int
+    public function test(): int
     {
-        // $courseGroup = CourseGroup::find(4);
-
-        // $courseGroup->courses_filtered = 'test';
-
-        // dump(
-        //     $courseGroup->coursesFiltered,
-        //     $courseGroup->toArray(),
-        // );
-
-        // return 1;
-
-        dump(
-            // $getCourseSelectDataService(
-            //     CourseGroupType::Specialization, 
-            //     Specialization::find(1), 
-            //     null,
-            // ),
-
-            $getCourseSelectDataService(
-                CourseGroupType::Specialization, 
-                Specialization::find(1), 
-                null,
-                true
-            )
-        );
-
         return 1;
     }
 }
