8fcb6a8dc201614c85580bd5af993f4f8f0f8a92
Florian Ratz <florian.ratz@supsign.ch>
2022-04-21 10:27:39 +0200

    no message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="container p-3 mx-auto">
         <div class="w-full sm:flex-grow">
             <x-base.card class="mb-4">
-                <div class="text-2xl">{{$introTitle}}</div>
+                <div class="text-2xl">{{ $introTitle }}</div>
                 <div class="flex flex-col">
                     <div class="whitespace-pre-line">
-                        {{ $introContent }}</div>
+                        {!! $introContent !!}</div>
                         <x-base.link href="{{ $introLink }}">{{ $introLink }}</x-base.link>
                 </div>
             </x-base.card>
