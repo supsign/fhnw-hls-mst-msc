2473b91ecb97af573b03a2270de2f76e2688c2a7
Florian Ratz <florian.ratz@supsign.ch>
2022-04-21 10:28:57 +0200

    formatting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,11 @@
                 <div class="text-2xl">{{ $introTitle }}</div>
                 <div class="flex flex-col">
                     <div class="whitespace-pre-line">
-                        {!! $introContent !!}</div>
-                        @if($introLink)
-                            <x-base.link href="{{ $introLink }}">{{ $introLink }}</x-base.link>
-                        @endif
+                        {!! $introContent !!}
+                    </div>
+                    @if($introLink)
+                        <x-base.link href="{{ $introLink }}">{{ $introLink }}</x-base.link>
+                    @endif
                 </div>
             </x-base.card>
             <livewire:module-selection-form :semesters="$semesters" :specializations="$specializations" :studyModes="$studyModes"/>
