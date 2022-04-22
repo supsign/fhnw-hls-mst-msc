cb584ecb7d0a89918c73c31745bf6ea99ecef8ca
Florian Ratz <florian.ratz@supsign.ch>
2022-04-22 10:01:17 +0200

    unbind radio buttons

--- resources/views/livewire/radio-group.blade.php
+++ resources/views/livewire/radio-group.blade.php
@@ -1,17 +1,17 @@
 <div class="border-r border-b flex gap-5">
     <div class="w-20 text-center">
-        <input type="radio" name="{{ $courseGroupId }}" wire:model="semesterId"/>
+        <input type="radio" name="{{ $courseGroupId }}"/>
     </div>
     
     @foreach($selectableSemesters AS $semester)
         <div class="w-20 text-center">
             @if($semester)
-                <input type="radio" name="{{ $courseGroupId }}" wire:model="semesterId" value="{{ $semester }}" id="{{ $semester }}">
+                <input type="radio" name="{{ $courseGroupId }}" value="{{ $semester }}" id="{{ $semester }}"/>
             @endif
         </div>
     @endforeach
     
     <div class="w-20 text-center">
-        <input type="radio" name="{{ $courseGroupId }}" wire:model="semesterId" value="later"/>
+        <input type="radio" name="{{ $courseGroupId }}" value="later"/>
     </div>
 </div>
