b05513c303e1d23293d27f6ecc1224efafec70c6
philipp.minder <philipp.minder@supsign.ch>
2022-04-26 15:27:37 +0200

    add tooltips

--- resources/views/livewire/course-group.blade.php
+++ resources/views/livewire/course-group.blade.php
@@ -12,15 +12,15 @@
         <div class="w-10 border-b"></div>
         <div class="flex gap-5 border-b">
             <div class="w-20 text-center">{{ !$further ? 'none' : '' }}</div>
-
             @foreach($nextSemesters AS $semester)
-                <div class="w-20 text-center ">{{ !$further ? $semester['short_name'] : '' }}</div>
+                <div class="relative"  x-data="{ hover: false }">
+                    <div class="w-20 text-center" x-on:mouseover="hover = true" x-on:mouseout="hover = false">{{ !$further ? $semester['short_name'] : '' }}</div>
+                    <x-tooltip x-show="hover" class="w-80">{{ $semester['tooltip'] }}</x-tooltip>
+                </div>
             @endforeach
-
             <div class="w-20 text-center {{ $further ? 'border-r': '' }}">{{ !$further ? 'later' : '' }}</div>
         </div>
     </div>
-
     @php
         $courses = $further ? $courseGroup['courses_filtered'] : $courseGroup['courses'];
     @endphp
@@ -28,6 +28,7 @@
         @foreach($courses as $course)
             <livewire:course
                 :courseGroupTypeShortName="$courseGroup['course_group_type_short_name']"
+                :courseGroupTypeTooltip="$courseGroup['course_group_type_tooltip']"
                 :course="$course"
                 :courseGroupId="$courseGroup['id']"
                 :further="$further"
