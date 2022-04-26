857cbb8fd4a536bf127ebfd79e58bdf8948d1b83
Florian Ratz <florian.ratz@supsign.ch>
2022-04-26 14:56:55 +0200

    overflächerlicher fix, muss livewire component werden

--- resources/views/components/optional-english.blade.php
+++ resources/views/components/optional-english.blade.php
@@ -1,4 +1,15 @@
 <div>
     <div class="mb-5 text-lg"><b>{{ $optionalEnglishTitle }}</b></div>
     <div>{!! $optionalEnglishDescription !!}</div>
+
+    @foreach($courses AS $course)
+        <livewire:course
+            :courseGroupTypeShortName="null"
+            :course="$course"
+            :courseGroupId="4"
+            :nextSemesters="[]"
+            :selectedCourses="[]"
+            key="{{ $course['id'] }}"
+        />
+    @endforeach
 </div>
