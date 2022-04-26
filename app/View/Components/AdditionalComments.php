893e50b1d5ecfa4889d56d12dad4977dd9fd76e7
Florian Ratz <florian.ratz@supsign.ch>
2022-04-26 11:30:18 +0200

    no message

--- app/View/Components/AdditionalComments.php
+++ app/View/Components/AdditionalComments.php
@@ -4,10 +4,10 @@ namespace App\View\Components;
 
 use App\Services\PageContents\PageContentService;
 use Illuminate\View\Component;
+use Illuminate\View\View;
 
 class AdditionalComments extends Component
 {
-
     public ?string $additionalCommentsTitle = null;
 
     public function __construct(protected PageContentService $pageContentService)
@@ -17,12 +17,7 @@ class AdditionalComments extends Component
         }
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
-    public function render()
+    public function render(): View
     {
         return view('components.additional-comments');
     }
