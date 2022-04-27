0225a314323c3999b409a67632f871ed47810198
philipp.minder <philipp.minder@supsign.ch>
2022-04-27 16:26:42 +0200

    save

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,3 +1,36 @@
 <div>
-    ITSE ME, PDF
+    <div class="2xl">Your Study Plan MSc in Life Sciences FHWN</div>
+    <div>GivenName Surname</div>
+    <div>Specialization</div>
+    <div>
+        <div>Semester 1</div>
+        <div class="flex">
+            <div>Module Title</div><div>Type</div><div>ECTS Venue</div>
+        </div>
+        <div class="flex">
+            <div>
+                Module
+            </div>
+            <div>
+                Type
+            </div>
+            <div>
+                3
+            </div>
+        </div>
+    </div>
+    <div>
+        <div>Master Thesis planned for StartDate to EndDate</div>
+        <div>BroadSubjectArea</div>
+        <div>FurtherDetailsOnMscTopic</div>
+    </div>
+    <div>
+        <div>Summary Statistics</div>
+        <div># of Specialization Modules</div>
+        <div># of Cluster-specific Modules</div>
+        <div># Core Competence Modules</div>
+        <div># Total number of ECTS</div>
+    </div>
+    <div>Please note that the module offer and the timing of the modules may change in the future.</div>
+    <div><div>Agreed, Date</div><div>Signature Student</div><div>Signature Director of Study Programme</div></div>
 </div>
