4c6eb8860724c3b3bacfd28cb984d1f9977217af
Florian Ratz <florian.ratz@supsign.ch>
2022-05-03 15:20:56 +0200

    wip pdfDataService

--- app/Services/GetPdfDataService.php
+++ app/Services/GetPdfDataService.php
@@ -2,16 +2,49 @@
 
 namespace App\Services;
 
+use App\Models\Semester;
+use App\Models\Specialization;
 use Illuminate\Http\Request;
 
 class GetPdfDataService
 {
+    protected array $data = [];
+
     public function __invoke(Request $request): array
     {
-        dd(
+        dump(
             $request->all()
         );
 
-        return [];
+        foreach ($request->all() AS $key => $value) {
+            switch ($key) {
+                case 'specialization':
+                    $this->data[$key] = Specialization::find($value);
+                    break;
+
+                case 'thesis_start':
+                    $this->data[$key] = Semester::find($value);
+                    break;
+
+                case 'selected_courses':
+                    $this->data[$key] = $this->getSelectedCourses($value);
+                    break;
+
+                default:
+                    $this->data[$key] = $value;
+                    break;
+            }
+        }
+
+        return $this->data;
+    }
+
+    protected function getSelectedCourses(array $selectedCourseData)
+    {
+
+        dump(
+            $selectedCourseData
+        );
+
     }
 }
