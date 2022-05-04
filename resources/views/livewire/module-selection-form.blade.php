31b888162ccd9d65ca14cda4c253f57410c11b0f
pminder <philipp.minder@hotmail.com>
2022-05-04 11:57:05 +0200

    add Statistic counts

--- resources/views/livewire/module-selection-form.blade.php
+++ resources/views/livewire/module-selection-form.blade.php
@@ -37,7 +37,7 @@
                 :selectedCourses="$selectedCourses['main'] ?? []"
             />
             <x-additional-comments />
-            <x-summary-statistics wire:model='statistics'/>
+            <livewire:summary-statistics  key="{{ now() }}" :statistics='$statistics'/>
             <div x-data>
                 <input type="submit" name="submit" value="Submit" class="button-primary" @click='window.scrollTo(0,0)'/>
             </div>
--- resources/views/livewire/summary-statistics.blade.php
+++ resources/views/livewire/summary-statistics.blade.php
@@ -1,5 +1,4 @@
 <div>
-    @dump($statistics)
     <div class="mb-5 text-lg"><b>Summary Statistics</b></div>
     <div>{{ $statistics['specialization'] ?? 0 }} of Specialization Modules</div>
     <div>{{ $statistics['cluster_specific'] ?? 0 }} of Cluster-specific Modules</div>
