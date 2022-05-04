85f4deed3365314da75f01a6360b2ad565249594
Florian Ratz <florian.ratz@supsign.ch>
2022-05-04 14:51:18 +0200

    no message

---
+++ app/Services/Courses/CheckOverlappingCoursesService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Courses;
+
+class CheckOverlappingCoursesService
+{
+    public function __invoke(array $selectedCourses)
+    {
+
+
+
+        return 'test';
+    }
+}
