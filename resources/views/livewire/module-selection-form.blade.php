24a12b6de48ad3838b6b2592117e532824d1fd5f
Florian Ratz <florian.ratz@supsign.ch>
2022-05-05 06:44:24 +0200

    no message

--- resources/views/livewire/module-selection-form.blade.php
+++ resources/views/livewire/module-selection-form.blade.php
@@ -37,7 +37,11 @@
                 :selectedCourses="$selectedCourses['main'] ?? []"
             />
             <x-additional-comments wire:model='additionalComments'/>
-            <livewire:summary-statistics  key="{{ microtime() }}" :statistics='$statistics' :ects='$ects'/>
+            <livewire:summary-statistics
+                key="{{ microtime() }}" 
+                :statistics='$statistics' 
+                :ects='$ects'
+            />
             <div x-data>
                 <input type="submit" name="submit" value="Submit" class="button-primary" @click='window.scrollTo(0,0)'/>
             </div>
