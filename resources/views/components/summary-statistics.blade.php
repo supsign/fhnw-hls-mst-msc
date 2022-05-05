e4885083ee6c17782e66cc12b165d72cf744eaa2
Florian Ratz <florian.ratz@supsign.ch>
2022-05-05 15:24:23 +0200

    weg mit alten component

--- resources/views/components/summary-statistics.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <div class="mb-5 text-lg"><b>Summary Statistics</b></div>
-    <div>{{ $statistics['specialization'] ?? 0 }} of Specialization Modules</div>
-    <div>{{ $statistics['cluster_specific'] ?? 0 }} of Cluster-specific Modules</div>
-    <div>{{ $statistics['core_compentences'] ?? 0 }} Core Competence Modules</div>
-</div>
