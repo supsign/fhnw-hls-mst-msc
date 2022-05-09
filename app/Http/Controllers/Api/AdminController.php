55046e7b94cd34d64639a26dff618d3cb518bf81
Florian Ratz <florian.ratz@supsign.ch>
2022-05-09 10:10:40 +0200

    no message

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PostConfiguration;
 use App\Services\ConfigurationImport;
-use stdClass;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
-    public function postConfiguration(PostConfiguration $request, ConfigurationImport $configurationImport) //: stdClass
+    public function postConfiguration(PostConfiguration $request, ConfigurationImport $configurationImport): RedirectResponse
     {
         $response = $configurationImport(
             $request->config_file->storeAs('config', $request->config_file->getClientOriginalName())
