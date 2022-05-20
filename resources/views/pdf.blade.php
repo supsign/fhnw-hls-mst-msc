95062cbaa5e47184ed278d22555dfb2e5a5bcd84
pminder <philipp.minder@hotmail.com>
2022-05-20 16:03:33 +0200

    sjpw pveröaüüomg

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -56,7 +56,7 @@
         @endisset
     </div>
     @if(!empty($modulesOutside))
-        <div style='margin-bottom: 1.25rem;'>
+        <div style='    margin-bottom: 1.25rem;'>
             <div style='font-size: 1.125rem; line-height: 1.75rem; font-weight: 700; margin-bottom: 0.75rem'>Modules outside the Curriculum</div>
             <table style='border-width: 1px; margin-bottom: 1.25rem; width: 100%; border-collapse: collapse;'>
                 <tr style='border: 1px solid black;'>
@@ -75,8 +75,6 @@
             </table>
         </div>
     @endif
-
-    <br />
     @isset($additionalComments)
         <div style='margin-bottom: 1.25rem;'>
             <div style='font-size: 1.125rem; line-height: 1.75rem; font-weight: 700; margin-bottom: 0.75rem'>Additional Comments</div>
@@ -84,6 +82,19 @@
         </div>
         <br />
     @endisset
+    @if($overlappingCourses->count())
+        <div style='font-size: 1.125rem; line-height: 1.75rem; font-weight: 700; margin-bottom: 0.75rem'>Overlapping Courses</div>
+        @foreach($overlappingCourses AS $semester)
+            <div style='font-weight: 700; margin-bottom: 0.25rem;'>{{ $semester->long_name }}</div>
+            @foreach($semester->overlappingCourses AS $courses)
+                <ul style='list-style-type: disc; list-style-position: inside; margin-bottom: 20px;'>
+                    @foreach ($courses AS $course)
+                        <li>{{ $course->name }}</li>
+                    @endforeach ($courses AS $course)
+                </ul>
+            @endforeach
+        @endforeach
+    @endif
     <div>
         <div style='font-size: 1.125rem; line-height: 1.75rem; font-weight: 700; margin-bottom: 0.75rem'>Summary Statistics</div>
         <div>Number of Specialization Modules: {{ $statistics['specialization'] }}</div>
@@ -98,27 +109,6 @@
     @endisset
     <br />
 
-    @if($overlappingCourses->count())
-        <div style='font-size: 1.125rem; line-height: 1.75rem; font-weight: 700; margin-bottom: 0.75rem'>Overlapping Courses</div>
-    @endif
-    @foreach($overlappingCourses AS $semester)
-        <div style='font-weight: 700; margin-bottom: 0.25rem;'>{{ $semester->long_name }}</div>
-        <table style='margin-bottom: 1.25rem; width: 100%; border-collapse: collapse;'>
-            <tr style='border: 1px solid black;'>
-                <th style='border-right: 1px solid black; padding: 0.25rem; width: 25rem; text-align: left'>Module Title</th>
-            </tr>
-            @foreach($semester->overlappingCourses AS $courses)
-                <tr style='border: 1px solid black;'>
-                    <th style='border-right: 1px solid black; padding: 0.25rem; width: 25rem; text-align: left'>{{ $courses->first()->slot->name }}</th>
-                </tr>
-                @foreach ($courses AS $course)
-                    <tr style='border: 1px solid black;'>
-                        <td style='border-right: 1px solid black; padding: 0.25rem; width: 25rem;'>{{ $course->name }}</td>
-                    </tr>
-                @endforeach ($courses AS $course)
-            @endforeach
-        </table>
-    @endforeach
 
     <br />
     <div >
